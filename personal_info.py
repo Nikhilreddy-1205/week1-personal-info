b35a6572ea682f8aa49c81b9147d02f6acb9b451
Nikhil Reddy <snikhilreddy999@gmail.com>
2025-12-17 14:40:57 +0530

    First commit: Week 1 personal info project

---
+++ personal_info.py
@@ -0,0 +1,24 @@
+print("hello world")
+print("this is nikhil reddy")
+print("    PERSONAL INFORMATION MANAGER")
+
+print()
+name = "nikhil"
+age = 21      
+city = "bangalore"
+print(" basic information:")
+print(30)
+print(f"Name: {"nikhil"}")
+print(f"Age: {21}")
+print(f"City: {"bangalore"}")
+
+favourite_food=input("enter your favourite food?")
+while favourite_food=="":
+    print("emter your favourite-food")
+    favourite_food=input("enter your favourite food?")
+
+
+favourite_place=input("enter favourite_place?")
+while favourite_place=="":
+    print("enter the place")
+    favourite_place=input("enter favourite_place?")
