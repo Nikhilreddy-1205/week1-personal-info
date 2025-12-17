16e37555ee05a0c59d2fe55e73250edce8f5ceb5
Nikhil Reddy <snikhilreddy999@gmail.com>
2025-12-17 14:52:56 +0530

    Update personal info logic

--- personal_info.py
+++ personal_info.py
@@ -5,12 +5,13 @@ print("    PERSONAL INFORMATION MANAGER")
 print()
 name = "nikhil"
 age = 21      
-city = "bangalore"
-print(" basic information:")
-print(30)
-print(f"Name: {"nikhil"}")
-print(f"Age: {21}")
-print(f"City: {"bangalore"}")
+city = "Bengaluru"
+print("basic information:")
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print()
 
 favourite_food=input("enter your favourite food?")
 while favourite_food=="":
@@ -22,3 +23,13 @@ favourite_place=input("enter favourite_place?")
 while favourite_place=="":
     print("enter the place")
     favourite_place=input("enter favourite_place?")
+
+age_in_months = age * 12
+
+print("\nYour information:")
+print("----------------------")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print(f"Favourite food: {favourite_food}")
+print(f"Favourite place: {favourite_place}")
